29547bb51efc210f155ec27f1b854a44845e7a49
flowerinthenight <root@flowerinthenight.com>
2024-06-20 00:08:28 +0900

    build seq

--- build.sh
+++ build.sh
@@ -2,10 +2,10 @@ echo "Compiling proto files..." && buf generate
 
 echo "Generating OpenAPI docs..."
 protoc -I . --openapiv2_out ./openapiv2 --openapiv2_opt logtostderr=true --openapiv2_opt allow_merge=true \
-       ./vortex/v1/*.proto \
-       ./iam/v1/*.proto
+       ./iam/v1/*.proto \
+       ./vortex/v1/*.proto
 
 echo "Generating html docs..."
 protoc --doc_out=./generated/ --doc_opt=html,index.html \
-       ./vortex/v1/*.proto \
-       ./iam/v1/*.proto
+       ./iam/v1/*.proto \
+       ./vortex/v1/*.proto

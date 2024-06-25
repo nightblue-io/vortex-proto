bdf5e7b5055f59debdf374757bd5ac5db4b65d8f
flowerinthenight <root@flowerinthenight.com>
2024-06-26 03:16:16 +0900

    fix build

--- build.sh
+++ build.sh
@@ -2,12 +2,14 @@ echo "Compiling proto files..." && buf generate
 
 echo "Generating OpenAPI docs..."
 protoc -I . --openapiv2_out ./openapiv2 --openapiv2_opt logtostderr=true --openapiv2_opt allow_merge=true \
+       types/*.proto \
        iam/v1/*.proto \
        vortex/v1/*.proto \
        operations/v1/*.proto
 
 echo "Generating html docs..."
 protoc --doc_out=./generated/ --doc_opt=html,index.html \
+       types/*.proto \
        iam/v1/*.proto \
        vortex/v1/*.proto \
        operations/v1/*.proto

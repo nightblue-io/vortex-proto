ea278140012c52b5fffe17e37952072ed1c97298
flowerinthenight <root@flowerinthenight.com>
2024-06-26 01:02:22 +0900

    add envs, longrunning

--- build.sh
+++ build.sh
@@ -2,10 +2,12 @@ echo "Compiling proto files..." && buf generate
 
 echo "Generating OpenAPI docs..."
 protoc -I . --openapiv2_out ./openapiv2 --openapiv2_opt logtostderr=true --openapiv2_opt allow_merge=true \
-       ./iam/v1/*.proto \
-       ./vortex/v1/*.proto
+       iam/v1/*.proto \
+       vortex/v1/*.proto \
+       longrunning/v1/*.proto
 
 echo "Generating html docs..."
 protoc --doc_out=./generated/ --doc_opt=html,index.html \
-       ./iam/v1/*.proto \
-       ./vortex/v1/*.proto
+       iam/v1/*.proto \
+       vortex/v1/*.proto \
+       longrunning/v1/*.proto
